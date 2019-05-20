d0e2bc4e322f157194ea9d957c3e6f858d563e1e
Elijah Florence <efloren@siue.edu>
2019-05-20 17:06:38 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,10 @@
 # Code your solution here!
+def run_guessing_game
+  number = gets
+  rand(10) = testNum
+  if number == testNum
+    puts("You guessesd the correct number!")
+  else
+    puts("The computer guessed #{number}.")
+  end
+end 
